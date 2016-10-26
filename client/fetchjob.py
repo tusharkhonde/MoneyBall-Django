dec06adbcbc1f502885bbb97cda5e617e1103d30
Tushar Khonde <tusharskhonde@gmail.com>
2016-10-26 14:55:47 -0700

    Minor changes

--- client/fetchjob.py
+++ client/fetchjob.py
@@ -4,35 +4,10 @@ import requests
 from client.models import *
 
 class FetchJob():
-    """
-    TODO:
 
-    Grabs data from the server and puts it in the database. This class is
-    instantiated/run periodically at an interval defined in fetchdata.py. Feel
-    free to check out the fetchdata command (in client/management/commands).
-    The API response formats are in the README. Be sure to handle both the
-    creation of new companies/metrics and the updating of existing ones. Be
-    mindful of foreign key dependencies.
-
-    For simplicity, you can assume uniqueness on the company name field and
-    join though it to get company metrics. Check the sample API response to
-    make sure you're parsing the data correctly.
-
-    You will be calling the following endpoints:
-    http://investmentiq.herokuapp.com/api/v1/companies/
-    http://investmentiq.herokuapp.com/api/v1/metrics/rand/
-
-    You might find the following helpful:
-    https://docs.djangoproject.com/en/1.7/ref/models/querysets/
-    """
 
     def run(self):
-        """
-        TODO:
 
-        Entry point for the FetchJob class. This should grab data from the
-        InvestmentIQ API and put it into the database.
-        """
         companies = requests.get('http://investmentiq.herokuapp.com/api/v1/companies/')
         companies = companies.json()
 
